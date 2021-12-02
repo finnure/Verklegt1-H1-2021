a4994b0ea46bbc8faa650200b5e795faf0fa30b8
kristinth <kristint21@ru.is>
2021-12-02 21:11:05 +0000

    Uppfæra buildings

--- src/models/building.py
+++ src/models/building.py
@@ -6,14 +6,21 @@ from structs.task import Task
 
 class Building():
 
-  def __init__(self, id: int, location_id: int, free: bool, description: str, state: str) -> None:
+  def __init__(self, id: int, building_id: int, location_id: int,  description: str, state: str, task: list, \
+    accessories: list, address: str, manager: str, reports: int, size: float, rooms: int, type: str) -> None:
     self.id = id
+    self.building_id = building_id
     self.location_id = location_id
-    self.free = free
     self.description = description
     self.state = state
+    self.task = []
     self.accessories = []
-    self.tasks = []
+    self.address = address
+    self.manager = manager
+    self.reports = reports
+    self.size = size
+    self.rooms = rooms
+    self.type = type
     
   def __str__(self) -> str:
     pass
@@ -23,3 +30,20 @@ class Building():
 
   def add_task(self, task: Type[Task]) -> None:
     self.tasks.append(task)
+  
+  def as_dict(self) -> 'dict[str, str | int]':
+    return {
+      'id' : self.id,
+      'building_id' : self.building_id,
+      'location_id' : self.location_id,
+      'description' : self.description,
+      'state' : self.state,
+      'task' : self.task, 
+      'accessories' : self.accessories, 
+      'address' : self.address, 
+      'manager' : self.manager,
+      'reports' : self.reports, 
+      'size' : self.size, 
+      'rooms' : self.rooms, 
+      'type' : self.type
+    }
