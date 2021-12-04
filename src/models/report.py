87229773f11188225622c44e7c9f34a34fa7cb88
kristinth <kristint21@ru.is>
2021-12-04 18:29:12 +0000

    laga kóða í models report

--- src/models/report.py
+++ src/models/report.py
@@ -1,11 +1,14 @@
 class Report():
 
-  def __init__(self, id: int, description: str, type: str, hours: int, cost: int) -> None:
+  def __init__(self, id: int,  report_date: str, approved: bool, task_type: str,\
+    description: str, note: str, hours: float) -> None:
     self.id = id
+    self.report_date = report_date
+    self.approved = approved
+    self.task_type = task_type
     self.description = description
-    self.type = type
+    self.note = note
     self.hours = hours
-    self.cost = cost
 
   def __str__(self) -> str:
     pass
@@ -13,20 +16,44 @@ class Report():
 class ContractorReport(Report):
 
   def __init__(self, id: int, contractor_id: int, employee_report_id: int,
-                description: str, type: str, hours: int, cost: int, rating: int) -> None:
-    super().__init__(id, description, type, hours, cost)
+                description: str,  hours: int, contractor_fee: float, contractor_rating: float) -> None:
+    super().__init__(id, approved, description, hours)
     self.contractor_id = contractor_id
     self.employee_report_id = employee_report_id
-    self.rating = rating
+    self.contractor_fee = contractor_fee
+    self.contractor_rating = contractor_rating
     
 class EmployeeReport(Report):
 
-  def __init__(self, id: int, employee_id: int, task_id: int, description: str, 
-                type: str, hours: int, cost: int) -> None:
-    super().__init__(id, description, type, hours, cost)
+  def __init__(self, id: int, employee_id: int, task_id: int, description: str, note: str,
+              hours: int, material_cost: float, labor_cost: float, total_cost: float) -> None:
+    super().__init__(id, description, report_date, approved, task_type, hours)
     self.employee_id = employee_id
     self.task_id = task_id
+    self.material_cost = material_cost
+    self.labor_cost = labor_cost
+    self.total_cost = total_cost
     self.contractor_reports = []
 
   def add_contractor_report(self, report: ContractorReport):
     self.contractor_reports.append(report)
+
+def as_dict(self) -> 'dict[str, str | int]':
+    return {
+      'id': self.id,
+      'report_date': self.report_date, 
+      'approved': self.approved, 
+      'task_type': self.task_type,
+      'description': self.description,
+      'note': self.note,
+      'hours': self.hours,
+      'contractor_id': self.contractor_id,
+      'employee_report_id': self.employee_report_id,
+      'contractor_fee': self.contractor_fee,
+      'contractor_rating': self.contractor_rating,
+      'employee_id': self.employee_id, 
+      'task_id': self.task_id, 
+      'material_cost': self.material_cost, 
+      'labor_cost': self.labor_cost, 
+      'contractor_reports': self.contractor_reports
+    }
